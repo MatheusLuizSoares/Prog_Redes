e352cf43954d254394dc6f7b9b5762ee91fd1461
Matheus Luiz Soares <matheusluiz15a@gmail.com>
2024-03-11 13:14:18 -0300

    q1 finalizado com sucesso

--- Q1/q1.py
+++ Q1/q1.py
@@ -1,41 +1,54 @@
-# Criando os parâmetros
-nome_arq_ori = input("Digite o nome do arquivo de origem: ")
-palavra_passe = input("Digite uma palavra-passe: ")
-nome_arq_dest = input("Digite o nome do arquivo de destino: ")
+###############################################################
+#                                                             #
+#  Author: @pedrohcdsouza & @MatheusLuizSoares                #
+#  Date: 10/03/2024                                           #
+#                                                             #
+###############################################################
 
-# criando um try
-try:
-  # Abrindo o arquivo no modo binário
-  with open(nome_arq_ori, "rb") as file_origem:
-        dado_ori = file_origem.read()
+# Verificando o diretório do programa.
+import os, sys                     
+strDir = os.path.abspath(__file__)  
+strDir = os.path.dirname(strDir)   
 
-    
-  try:
-    with open(nome_arq_dest, "xb") as file_destino:
-      pass
-  # Criando um except para que não haja arquivo repetido
-  except FileExistsError:
-        print("O arquivo já existe. Escolha um novo nome.")
-  else:
-    #vai saber o tamanho
-     tam_palavra_passe = len(palavra_passe)
-     
-       # Um bytearray é um metodo semelhante a uma lista, mas é mutável, permitindo a modificação dos valores dos bytes.
-     dado_cript = bytearray()
-     
-    #O for vai ser criado para que obtenha tanto os valores de i e do byte,por isso o uso do enumemerate
-     for i, byte in enumerate(dado_ori):
-            byte_palavra_passe = ord(palavra_passe[i % tam_palavra_passe])
-            byte_cripto = byte ^ byte_palavra_passe
-            dado_cript.append(byte_cripto)
-     #a gente vai modificar o arquivo por isso ouso "wb"
-     with open(nome_arq_dest, "wb") as file_destino:
-            file_destino.write(dado_cript)
-            print("A criptografia foi concluída com sucesso")
+# Criando um laço de repetição para solicitar o nome do arquivo (e abrindo-o), e a palavra-chave desejada. Tratando as exceções caso necessário.
+while True: 
+    try:
+        ArcName = str(input("Please enter the desired filename (including the extension): "))
+        ArcDir = os.path.join(strDir, ArcName)
+        with open(ArcDir,'rb') as Arc_rb:
+            ArcData = Arc_rb.read()
+        Keyword = str(input("Please enter a keyword: "))
+        KeyLen = len(Keyword)
+    except FileNotFoundError:
+        print("\nThe desired file was not found!\nTry again...\n")
+    except:
+        print(f"\nERROR: {sys.exc_info()[0]}")
+        sys.exit()
+    else:
+        break
 
-except FileNotFoundError:
-    print("Arquivo de origem não encontrado")
-except Exception as e:
-    print(f"Ocorreu um erro: {e}")
-#finalizando o codigo
-#o codigo pode ser visto atraves do 
+# Verificando a posição atual byte do texto, e do byte da palavra-chave. E utilizando o operador xor (^).
+BytesList = []
+for i, byte in enumerate(ArcData):
+    byte_password = ord(Keyword[i % KeyLen])
+    BytesList.append(byte ^ byte_password)
+
+# Criando um laço de repetição para solicitar o nome do arquivo (e cria-lo), escrevendo no arquivo cada byte 'criptografado' que foi colocado dentro da lista (BytesList). Tratando as exceções caso necessário.
+while True:
+    try:
+        ArcDest = str(input("Please enter the destination file name: "))
+        ArcDestDir = os.path.join(strDir, ArcDest)
+
+        with open(ArcDestDir, 'wb') as Arc_wb:
+            for byte in BytesList:
+                Arc_wb.write(byte.to_bytes(1, byteorder='big'))
+    except FileNotFoundError:
+        print("\nThe file name is invalid!\n")
+        usercommand = str(input("\nPress 0 -> Exit\nPress 1 -> Return\n"))
+        if usercommand == 0:
+            sys.exit()
+    except:
+        print(f"\nERROR: {sys.exc_info()[0]}")
+        sys.exit()
+    else:
+        break
