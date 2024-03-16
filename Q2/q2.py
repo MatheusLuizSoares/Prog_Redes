3cc6b67b18d12af58d0e71f6d44df9d16e2a5bc6
Matheus Luiz Soares <matheusluiz15a@gmail.com>
2024-03-16 18:40:14 -0300

    finalizado

--- Q2/q2.py
+++ Q2/q2.py
@@ -5,31 +5,13 @@
 #                                                             #
 ###############################################################
 
+import sys
+from mylib import *
 
-
-import time, hashlib
-
-def findNonce(dataToHash, bitsToBeZero):
-    print('Código em execução... ')
-#iniciando  o contador atraves do time 
-    StartTime = time.time()
-
-    nonce = 0
-    while True:
-        h = hashlib.sha256()
-        dataBytes = dataToHash.encode('utf-8')  # Convertendo a string em bytes
-        nonceBytes = nonce.to_bytes(4, byteorder='big')
-        h.update(dataBytes + nonceBytes)
-        h_bin = ''.join(format(byte, '08b') for byte in h.digest())
-
-        if h_bin[0:int(bitsToBeZero)] == '0'*int(bitsToBeZero):#convertendo bitstobezero para inteiros
-            break
-        else:
-            nonce += 1
-            print(nonce)
-
-    EndTime = time.time()
-
-    print(nonce)
-    print(f'O tempo foi: {round(EndTime-StartTime,2)} segundos.')
-    print(h.hexdigest())
+if len(sys.argv) == 3:
+    Nonce, ProgramTime, HashHex = findNonce(sys.argv[1],sys.argv[2])
+    print(f'Number of attempts: {Nonce} Nonces.')
+    print(f'Program time: {ProgramTime} Seconds.')
+    print(f'Hashlib hexa: {HashHex}.')
+else:
+    print("\nPlease enter 2 arguments.\n")
